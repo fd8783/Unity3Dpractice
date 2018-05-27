5e6d21fd1ba1ed26b79d0bfc0fe62ebda1224a56
fd8783 <30581760+fd8783@users.noreply.github.com>
2018-05-27 14:37:17 +0800

    m

---
+++ 3Dpractice/Assets/Script/IKCtrl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKCtrl : MonoBehaviour {
+
+	public float rightHandIKWeight;
+
+	public Transform targetRightHand, targetRightHint;
+
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Awake () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnAnimatorIK (int layerIndex)
+	{
+		anim.SetIKPositionWeight(AvatarIKGoal.RightHand, rightHandIKWeight);
+		anim.SetIKRotationWeight(AvatarIKGoal.RightHand, rightHandIKWeight);
+		anim.SetIKPosition(AvatarIKGoal.RightHand, targetRightHand.position);
+		anim.SetIKRotation(AvatarIKGoal.RightHand, targetRightHand.rotation);
+		anim.SetIKHintPosition(AvatarIKHint.RightElbow, targetRightHint.position);
+		anim.SetIKHintPositionWeight(AvatarIKHint.RightElbow, rightHandIKWeight);
+
+	}
+}
---
+++ 3Dpractice/Assets/Script/IKCtrlEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(IKCtrl))]
+public class IKCtrlEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		IKCtrl ctrl = (IKCtrl)target;
+
+		ctrl.rightHandIKWeight = EditorGUILayout.Slider("RightHandIKWeight",ctrl.rightHandIKWeight, 0f, 1f);
+	}
+}
